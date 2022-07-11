85f8c7aa20c8cd116540ff9fd6f66cd981b76b26
theodotcom <61735898+theodotcom@users.noreply.github.com>
2022-07-11 18:05:53 +0200

    log free

--- scripts/api.js
+++ scripts/api.js
@@ -1,13 +1,13 @@
 export async function getRecipes() {
-    const fetchData = await fetch('data/recipe.json')
-        .then((response) => {
-            return response.json()
-        })
-        .then((data) => {
-            return data;
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-    return fetchData
+  const fetchData = await fetch("data/recipe.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  return fetchData;
 }
